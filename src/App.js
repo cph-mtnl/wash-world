c455800fe51e1249da0af3558faf672bdca318b7
cph-mtnl <mtnl@cphbusiness.dk>
2022-04-12 22:49:39 +0200

    my solution

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import info from "./info";
-import WashLocation from "./components/WashLocation";
+import { useState, useEffect } from "react";
+import WashLocations from "./views/WashLocations";
+import NavBar from "./components/NavBar";
+import "./styles/app.css";
+import WashProducts from "./views/WashProducts";
+import WashInProgress from "./views/WashInProgress";
 
 function App() {
-  const [locations, setLocations] = useState([]);
+  const [view, setView] = useState("locations");
+  const [location, setLocation] = useState(undefined);
+  const [product, setProduct] = useState(undefined);
 
-  useEffect(() => {
-    axios.get(info.backendUrl + "/locations").then((result) => {
-      setLocations(result.data.response.locations);
-    });
-  }, []);
+  function handleSetLocation(location) {
+    setView("products");
+    setLocation(location);
+  }
+
+  function handleSetProduct(product) {
+    setView("in progress");
+    setProduct(product);
+  }
 
   return (
     <div>
-      {locations.map((location) => {
-        return <WashLocation key={location.id} location={location} />;
-      })}
+      <NavBar />
+      <div className="content">
+        {view === "locations" && (
+          <WashLocations setSelectedLocation={handleSetLocation} />
+        )}
+        {view === "products" && location && (
+          <WashProducts
+            location={location}
+            setSelectedProduct={handleSetProduct}
+          />
+        )}
+        {view === "in progress" && location && product && (
+          <WashInProgress location={location} product={product} />
+        )}
+      </div>
     </div>
   );
 }
--- src/info.js
+++
@@ -1,5 +0,0 @@
-const info = {
-  backendUrl: "https://b46f027d-3a5f-4de6-9075-5e861759e531.mock.pstmn.io",
-};
-
-export default info;
